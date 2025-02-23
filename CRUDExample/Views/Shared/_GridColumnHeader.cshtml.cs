356d80f3efa45b27572b1fdf77c5f2ce0588322b
Uladzislau Bainiarovich <118025624+Vlad16d@users.noreply.github.com>
2025-02-23 21:05:47 +0100

    Add files via upload

---
+++ CRUDExample/Views/Shared/_GridColumnHeader.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRUDExample.Views.Shared
+{
+    public class _GridColumnHeaderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
